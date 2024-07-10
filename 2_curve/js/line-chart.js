c16f2f2ae7f1f13681fc1a9f09e7ea91eaa1fae4
gguex <guillaume.guex@gmail.com>
2024-07-10 11:15:04 +0200

    add book scripts

--- 2_curve/js/line-chart.js
+++ 2_curve/js/line-chart.js
@@ -1,4 +1,8 @@
 // Load the data here
+d3.csv("/2_curve/data/weekly_temperature.csv", d3.autoType)
+  .then(data => {
+    console.log("temperature data", data);
+  });
 
 // Create the line chart here
 const drawLineChart = (data) => {
